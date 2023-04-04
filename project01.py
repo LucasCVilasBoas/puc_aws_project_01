afc46a3e159b7cf82aaf38a6fb33945010aa6f55
LucasCVilasBoas <47900896+LucasCVilasBoas@users.noreply.github.com>
2023-04-04 18:04:08 -0300

    Code refactor to improve readability

--- project01.py
+++ project01.py
@@ -1,8 +1,12 @@
-import requests
+from requests import request
 
-url_valid_isbn13 = "http://webservices.daehosting.com/services/isbnservice.wso"
+headers = {"Content-Type": "text/xml; charset=utf-8"}
 
-payload_valid_isbn13 = """<?xml version="1.0" encoding="utf-8"?>
+
+# Checa se um ISBN13 é válido
+url_isbn13 = "http://webservices.daehosting.com/services/isbnservice.wso"
+
+payload_isbn13 = """<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
     <IsValidISBN13 xmlns="http://webservices.daehosting.com/ISBN">
@@ -12,9 +16,14 @@ payload_valid_isbn13 = """<?xml version="1.0" encoding="utf-8"?>
 </soap:Envelope>
 """
 
-url_multiply = "http://www.dneonline.com/calculator.asmx"
+response_isbn13 = request("POST", url_isbn13, headers=headers, data=payload_isbn13)
+print(response_isbn13.status_code)
 
-payload_multiply = """<?xml version="1.0" encoding="UTF-8"?>
+
+# Efetua a multiplicação de 2 números inteiros
+url_mult = "http://www.dneonline.com/calculator.asmx"
+
+payload_mult = """<?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
    <soap:Body>
       <Multiply xmlns="http://tempuri.org/">
@@ -25,7 +34,11 @@ payload_multiply = """<?xml version="1.0" encoding="UTF-8"?>
 </soap:Envelope>
 """
 
+response_mult = request("POST", url_mult, headers=headers, data=payload_mult)
+print(response_mult.status_code)
 
+
+# Retorna uma lista dos países do mundo com seus respectivos códigos ISO
 url_country = (
     "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
 )
@@ -39,11 +52,16 @@ payload_country = """<?xml version="1.0" encoding="utf-8"?>
 </soap12:Envelope>
 """
 
-url_list_currencies = (
+response_countries = request("POST", url_country, headers=headers, data=payload_country)
+print(response_countries.status_code)
+
+
+# Retorna uma lista das moedas do mundo com seus respectivos códigos ISO
+url_currency = (
     "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
 )
 
-payload_list_currencies = """<?xml version="1.0" encoding="utf-8"?>
+payload_curr = """<?xml version="1.0" encoding="utf-8"?>
 <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
   <soap12:Body>
     <ListOfCurrenciesByName xmlns="http://www.oorsprong.org/websamples.countryinfo">
@@ -52,8 +70,13 @@ payload_list_currencies = """<?xml version="1.0" encoding="utf-8"?>
 </soap12:Envelope>
 """
 
-url_convert_temperature = "http://webservices.daehosting.com/services/temperatureconversions.wso?op=FahrenheitToCelsius"
-payload_convert_temperature = """<?xml version="1.0" encoding="utf-8"?>
+response_currency = request("POST", url_currency, headers=headers, data=payload_curr)
+print(response_currency.status_code)
+
+
+# Converte uma temperatura em Fahrenheit para Celsius
+url_temp = "http://webservices.daehosting.com/services/temperatureconversions.wso?op=FahrenheitToCelsius"
+payload_temp = """<?xml version="1.0" encoding="utf-8"?>
 <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
   <soap12:Body>
     <FahrenheitToCelsius xmlns="http://webservices.daehosting.com/temperature">
@@ -63,33 +86,5 @@ payload_convert_temperature = """<?xml version="1.0" encoding="utf-8"?>
 </soap12:Envelope>"""
 
 
-headers = {"Content-Type": "text/xml; charset=utf-8"}
-
-response_multiply = requests.request(
-    "POST", url_multiply, headers=headers, data=payload_multiply
-)
-print(response_multiply.status_code)
-
-
-response_list_country = requests.request(
-    "POST", url_country, headers=headers, data=payload_country
-)
-print(response_list_country.status_code)
-
-
-response_list_currencies = requests.request(
-    "POST", url_list_currencies, headers=headers, data=payload_list_currencies
-)
-print(response_list_currencies.status_code)
-
-
-response_convert_temperature = requests.request(
-    "POST", url_convert_temperature, headers=headers, data=payload_convert_temperature
-)
-print(response_convert_temperature.status_code)
-
-
-response_valid_isbn13 = requests.request(
-    "POST", url_valid_isbn13, headers=headers, data=payload_valid_isbn13
-)
-print(response_valid_isbn13.status_code)
+response_temp = request("POST", url_temp, headers=headers, data=payload_temp)
+print(response_temp.status_code)
